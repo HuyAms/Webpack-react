4b823551a47a81210a5af8f28cafc2daa1071c48
HuyTrinh <huy@makehelsinki.com>
2018-10-19 10:20:44 +0300

    Split environment config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-module.exports = ({mode}) => {
-  return {
-    mode,
-    output: {
-      filename: "bundle.js"
-    }
-  }
-}
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const webpackMerge = require("webpack-merge");
+
+module.exports = ({mode} = {mode: "production"}) =>
+    webpackMerge({
+      mode,
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [
+        new HtmlWebpackPlugin()
+      ]
+    },
+        modeConfig(mode))
